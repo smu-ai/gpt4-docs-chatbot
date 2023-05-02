9ceb51590811bc6c459395b21d1a0194f94283df
Donghao Huang <donghao.huang@mastercard.com>
2023-05-02 12:57:04 +0800

    added netlify edge function /chat

---
+++ netlify/_shared/makechain.ts
@@ -0,0 +1,42 @@
+import { OpenAIChat } from 'langchain/llms/openai';
+import { VectorStore } from 'langchain/vectorstores/base';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+import { CallbackManager } from 'langchain/callbacks';
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
+{context}
+
+Question: {question}
+Helpful answer in markdown:`;
+
+export const makeChain = (vectorstore: VectorStore,
+  callbackManager: CallbackManager
+) => {
+  const model = new OpenAIChat({
+    temperature: 0,
+    modelName: 'gpt-4', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+    streaming: Boolean(callbackManager),
+    callbackManager
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+    },
+  );
+  return chain;
+};
