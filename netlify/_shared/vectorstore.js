f11a7b3bba321fe26e4af5ca585723113181058d
Donghao Huang <donghao.huang@mastercard.com>
2023-05-03 09:16:02 +0800

    Update vectorstore.js

--- netlify/_shared/vectorstore.js
+++ netlify/_shared/vectorstore.js
@@ -1,9 +1,8 @@
 import { PineconeStore } from 'langchain/vectorstores';
-import { HNSWLib } from 'langchain/vectorstores/hnswlib';
-import { Chroma } from 'langchain/vectorstores/chroma';
-// import { Milvus } from 'langchain/vectorstores/milvus';
-import { Embeddings } from 'langchain/embeddings/base';
 import { PineconeClient } from '@pinecone-database/pinecone';
+import { Chroma } from 'langchain/vectorstores/chroma';
+import { Milvus } from 'langchain/vectorstores/milvus';
+import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 
 async function initPinecone(env) {
   if (!env.get('PINECONE_ENVIRONMENT') || !env.get('PINECONE_API_KEY')) {
@@ -26,19 +25,13 @@ async function initPinecone(env) {
 }
 
 export const initVectorStore = async (embeddings, env) => {
-  const PINECONE_INDEX_NAME = env.get('PINECONE_INDEX_NAME');
-  const PINECONE_NAME_SPACE = env.get('PINECONE_NAME_SPACE');
   const VECTOR_STORE = env.get('VECTOR_STORE');
-  const HNSWLIB_DB_DIR = env.get('HNSWLIB_DB_DIR');
-  const CHROMA_SERVER_URL = env.get('CHROMA_SERVER_URL');
-  const CHROMA_COLLECTION_NAME = env.get('CHROMA_COLLECTION_NAME');
-  // const MILVUS_SERVER_URL = env.get('MILVUS_SERVER_URL');
-  // const MILVUS_COLLECTION_NAME = env.get('MILVUS_COLLECTION_NAME');
-  // const MILVUS_DB_USERNAME = env.get('MILVUS_DB_USERNAME');
-  // const MILVUS_DB_PASSWORD = env.get('MILVUS_DB_PASSWORD');
+  console.log('initializing vector store: ', VECTOR_STORE);
 
   let vectorStore
   if (VECTOR_STORE === 'pinecone') {
+    const PINECONE_INDEX_NAME = env.get('PINECONE_INDEX_NAME');
+    const PINECONE_NAME_SPACE = env.get('PINECONE_NAME_SPACE');
     const pinecone = await initPinecone(env);
     const index = pinecone.Index(PINECONE_INDEX_NAME);
 
@@ -52,21 +45,30 @@ export const initVectorStore = async (embeddings, env) => {
       },
     );
   } else if (VECTOR_STORE === 'chroma') {
+    const CHROMA_SERVER_URL = env.get('CHROMA_SERVER_URL');
+    const CHROMA_COLLECTION_NAME = env.get('CHROMA_COLLECTION_NAME');
+
     vectorStore = await Chroma.fromExistingCollection(embeddings,
       {
         collectionName: CHROMA_COLLECTION_NAME,
         url: CHROMA_SERVER_URL,
       });
-    // } else if (VECTOR_STORE === 'milvus') {
-    //   vectorStore = await Milvus.fromExistingCollection(embeddings,
-    //     {
-    //       collectionName: MILVUS_COLLECTION_NAME!,
-    //       url: MILVUS_SERVER_URL!,
-    //       username: MILVUS_DB_USERNAME,
-    //       password: MILVUS_DB_PASSWORD,
-    //       ssl: MILVUS_SERVER_URL!.startsWith('https')
-    //     });
+  } else if (VECTOR_STORE === 'milvus') {
+    const MILVUS_SERVER_URL = env.get('MILVUS_SERVER_URL');
+    const MILVUS_COLLECTION_NAME = env.get('MILVUS_COLLECTION_NAME');
+    const MILVUS_DB_USERNAME = env.get('MILVUS_DB_USERNAME');
+    const MILVUS_DB_PASSWORD = env.get('MILVUS_DB_PASSWORD');
+
+    vectorStore = await Milvus.fromExistingCollection(embeddings,
+      {
+        collectionName: MILVUS_COLLECTION_NAME!,
+        url: MILVUS_SERVER_URL!,
+        username: MILVUS_DB_USERNAME,
+        password: MILVUS_DB_PASSWORD,
+        ssl: MILVUS_SERVER_URL!.startsWith('https')
+      });
   } else {
+    const HNSWLIB_DB_DIR = env.get('HNSWLIB_DB_DIR');
     vectorStore = await HNSWLib.load(HNSWLIB_DB_DIR, embeddings);
   }
 
