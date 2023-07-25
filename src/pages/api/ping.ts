905cb3bd1bd26202381251e4eebf8224352151e9
Donghao Huang <donghao.huang@mastercard.com>
2023-07-25 11:09:16 +0800

    added api/ping

---
+++ src/pages/api/ping.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { initPinecone } from '@/utils/pinecone-client';
+import { PINECONE_INDEX_NAME } from '@/config/pinecone';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    console.log('pining from: ', req.ip);
+    //only accept GET requests
+    if (req.method !== 'GET') {
+        res.status(405).json({ error: 'Method not allowed' });
+        return;
+    }
+
+    const pinecone = await initPinecone();
+    const index = pinecone.Index(PINECONE_INDEX_NAME!); //change to your own index name
+    const indexData = await index.describeIndexStats({
+        describeIndexStatsRequest: {},
+    });
+
+    console.log('PING response: ', indexData);
+    res.json(indexData);
+}
